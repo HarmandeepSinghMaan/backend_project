e7b5af437f9d211ccc44c84621c23da3b796f61e
Harmandeep-2005 <159959009+Harmandeep-2005@users.noreply.github.com>
2025-04-25 22:50:24 +0530

    video and other

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema=new mongoose.Schema({
   videoFile:{
@@ -40,5 +41,5 @@ const videoSchema=new mongoose.Schema({
 
 },{timestams:true})
 
-
+videoSchema.plugin(mongooseAggregatePaginate)
 export const Video=mongoose.model("Video",videoSchema)
